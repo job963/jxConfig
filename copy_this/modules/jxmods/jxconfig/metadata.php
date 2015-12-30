dfb4096de3e0c0a440b12fcf4d769da0a72c7230
job963 <jobarthel@gmail.com>
2015-12-30 15:00:16 +0100

    Metadata.php updated

--- copy_this/modules/jxmods/jxconfig/metadata.php
+++ copy_this/modules/jxmods/jxconfig/metadata.php
@@ -11,11 +11,11 @@ $aModule = array(
     'id'           => 'jxconfig',
     'title'        => 'jxConfig - Display of Shop Configuration',
     'description'  => array(
-                        'de' => 'Anzeige der protokollierten Admin Aktionen an jedem Objekt und als Gesamtbericht.',
-                        'en' => 'Display of Logged Administrative Actions for each Object and as complete Report.'
+                        'de' => 'Anzeige der Shop- und Moduleinstellungen als Gesamtbericht.',
+                        'en' => 'Display of shop and module settings as complete report.'
                         ),
     'thumbnail'    => 'jxconfig.png',
-    'version'      => '0.1.1',
+    'version'      => '0.1.2',
     'author'       => 'Joachim Barthel',
     'url'          => 'https://github.com/job963/jxConfig',
     'email'        => 'jobarthel@gmail.com',
